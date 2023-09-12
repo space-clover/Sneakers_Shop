84e2f27270d99652f75851c1b1fa2e7c7b39e850
Jonathan Mejia <jefry.jonathan.quintanilla@gmail.com>
2023-09-11 21:46:06 -0600

    responsive to large screen features added

--- app/Components/Nav_section.jsx
+++ app/Components/Nav_section.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav_section = () => {
     return (
-        <nav className='flex flex-col justify-between text-blacktosummer sticky left-2 top-16 w-0 z-10  h-85vh'>
+        <nav className='flex flex-col justify-between text-blacktosummer sticky left-3 top-16 w-0 z-10  h-85vh'>
             <h1 className='text-xl font-bold -rotate-90'>SUMMER SPRING</h1>
             <ul className='flex- flex-col text-light text-md'>
                 <li>SHOP</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1340px',
+    },
     rotate: {
       '0': '0deg',
       '45': '45deg',
